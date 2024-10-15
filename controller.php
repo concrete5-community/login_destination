9053465dd9bd08a48e2c55e1be39f4df80ac67b7
Michele Locati <michele@locati.it>
2024-10-15 09:04:39 +0200

    Version 1.2.1

--- controller.php
+++ controller.php
@@ -36,7 +36,7 @@ class Controller extends Package implements ProviderAggregateInterface
      *
      * @var string
      */
-    protected $pkgVersion = '1.2.0';
+    protected $pkgVersion = '1.2.1';
 
     /**
      * {@inheritdoc}
